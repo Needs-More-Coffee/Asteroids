eec1572015ac6dc449c69c006e6122930678acb2
Needs-More-Coffee <tjwood1996@gmail.com>
2025-11-12 21:04:02 -0600

    This was so irritating, had to rewrite everything.

--- main.py
+++ main.py
@@ -1,54 +1,60 @@
 import sys
+
 import pygame
-from constants import *
-from logger import log_state
-from logger import log_event
-from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_event, log_state
+from player import Player
 from shot import Shot
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    running = True
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    shot = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
-    Asteroid.containers = (updatable, drawable, asteroids)
-    AsteroidField.containers = (updatable,)
-    Shot.containers = (updatable, drawable, shot)
-    player = Player(x, y, PLAYER_RADIUS)
-    asteroidfield = AsteroidField()
 
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    while running:
-        for event in pygame.event.get():
-            if  event.type == pygame.QUIT:
-                running = False
-            
+    dt = 0
+
+    while True:
         log_state()
-        screen.fill("black")
-        for thing in drawable:
-            thing.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         updatable.update(dt)
-        for stuff in asteroids:
-            if player.collides_with(stuff):
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
                 log_event("player_hit")
-                print("GAME OVER!")
+                print("Game over!")
                 sys.exit()
-        
-        
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
